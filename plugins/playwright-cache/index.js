ff9f1bf6ec7987f9ea8a40337e5a6f359115df08
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-09-08 22:07:08 +0700

    chore: use netlify-plugin-playwright-cache

--- plugins/playwright-cache/index.js
+++ plugins/playwright-cache/index.js
@@ -1,3 +1,6 @@
+// This is not being used anymore.
+// Instead, we use https://github.com/nvh95/netlify-plugin-playwright-cache directly
+
 // https://mxb.dev/blog/persistent-build-folders-netlify/
 module.exports = {
   // Before the build runs,
@@ -6,13 +9,13 @@ module.exports = {
   //  - the directory already exists locally
   //  - the directory has never been cached
   async onPreBuild({ utils }) {
-      await utils.cache.restore('/opt/buildhome/.cache/ms-playwright')
+    await utils.cache.restore("/opt/buildhome/.cache/ms-playwright");
   },
   // After the build is done,
   // cache directory for future builds.
   // Does not do anything if:
   //  - the directory does not exist
   async onPostBuild({ utils }) {
-      await utils.cache.save('/opt/buildhome/.cache/ms-playwright')
-  }
-}
+    await utils.cache.save("/opt/buildhome/.cache/ms-playwright");
+  },
+};
